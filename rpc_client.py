0091e7d6b813d4f76bff855eb5f396bc09a14e1e
caoyongchao <caoyongchao@bytedance.com>
2020-10-31 11:22:56 +0800

    rpc service test ok
    Change-Id: I482e41d9addcf85e9703747f853802703ae6b083

--- rpc_client.py
+++ rpc_client.py
@@ -1,5 +1,32 @@
 from xmlrpc.client import ServerProxy
 
-if __name__ == '__main__':
+def cpabe_enc_cli(plain, policy):
     server = ServerProxy("http://localhost:8888") # 初始化服务器
-    print (server.get_string("oldboy_python6666")) # 调用函数并传参
+    # print (server.get_string("oldboy_python6666")) # 调用函数并传参
+    ct = server.cpabe_enc_cli(plain, policy)
+    print("ct = ", ct)
+    return ct.data
+def cpabe_dec_cli(ct, username):
+    server = ServerProxy("http://localhost:8888") # 初始化服务器
+    # print (server.get_string("oldboy_python6666")) # 调用函数并传参
+    pt = server.cpabe_dec_cli(ct, username)
+    return pt.data
+def cpabe_userkey_cli(username, userattri):
+    server = ServerProxy("http://localhost:8888")  # 初始化服务器
+    # print (server.get_string("oldboy_python6666")) # 调用函数并传参
+    sk = server.cpabe_usrkey(username, userattri)
+    return sk.data   # whether sk is returned, think lator;
+
+if __name__ == '__main__':
+    plain = "hello rpc"
+    policy = "Dept:sec"
+    ct = cpabe_enc_cli(plain, policy)
+    print("ct type:", type(ct))
+    with open("./pt.txt", "wb") as f:
+        f.write(ct)
+        f.close()
+
+    sk = cpabe_userkey_cli("cyc1","Dept:sec")
+    print("sk = ", sk)
+    pt = cpabe_dec_cli(ct, "cyc1")
+    print("pt = ", pt)
--- rpc_server.py
+++ rpc_server.py
@@ -1,12 +1,62 @@
+from __future__ import print_function
+import pyopenabe
 from xmlrpc.server import SimpleXMLRPCServer
 
 # 调用函数
 def respon_string(str):
     return "get string:%s"%str
 
+class cpabe():
+    def __init__(self):
+        self.openabe = pyopenabe.PyOpenABE()
+        self.cpabe = self.openabe.CreateABEContext("CP-ABE")
+        self.cpabe.generateParams()
+    def enc(self, message, policy):
+        print("enc service is called.")
+        ct = self.cpabe.encrypt(policy, message)
+        return ct
+    def dec(self, ct, username):
+        key_path = "./keys/" + username + "_sk.txt"
+        with open(key_path, "rb") as f:
+            sk = f.read()
+            f.close()
+        self.cpabe.importUserKey(username, sk)
+        print("ddkdk, ", type(ct), type(username))
+        try:
+            pt = self.cpabe.decrypt(username, ct.data)   # 解析返回错误，高优
+            print("service dec pt =", pt)
+            if pt == 0: return "DEC FAIL"
+            return pt
+
+        except Exception as e:
+            print(e)
+            return "DEC FAIL0"
+    def generate_userkey(self, username, userattri):
+
+        try:
+            self.cpabe.keygen(userattri, username)
+        except:
+            return "GENKEY FAIL"
+        uk = self.cpabe.exportUserKey(username)
+        # should write to db;
+        filepath = "./keys/" + username + "_sk.txt"
+
+        with open(filepath, 'wb') as f:
+            f.write(uk)
+            f.close()
+        return uk
+
+
+
+
 
 if __name__ == '__main__':
     server = SimpleXMLRPCServer(('localhost', 8888)) # 初始化
     server.register_function(respon_string, "get_string") # 注册函数
+    alg = cpabe()
+    server.register_function(alg.enc, "cpabe_enc_cli")
+    server.register_function(alg.dec, "cpabe_dec_cli")
+    server.register_function(alg.generate_userkey, "cpabe_usrkey")
+
     print ("Listening for Client")
     server.serve_forever() # 保持等待调用状态
